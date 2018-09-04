72159a28564b7ad8407ad94d5b22c7387ea8a1b8
Kyle Hughes <kylehughes1@gmail.com>
2018-09-04 13:02:06 -0500

    renamed audio_test to main and left example code at bottom; needs to create new Noise obj each time it is played since context manager closes the stream

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ apt install omxplayer ffmpeg libavcodec-extra-53 python3-pyaudio
 echo 'options snd-card-usb-caiaq index=0' > /etc/modprobe.d/alsa-base.conf
 chown root:root /etc/modprobe.d/alsa-base.conf
 chmod 644 /etc/modprobe.d/alsa-base.conf
+
+# darwin only
+# brew install portaudio
+# otherwise make sure portaudio is installed if pip won't install pyaudio
+# any OS other than raspi
+# pip install --user pyaudio
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 
 from itertools import cycle
+from os import scandir
 from pyaudio import PyAudio
-import wave  
+import wave
 
 
 class Machine(PyAudio):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-    
+
     def __enter__(self):
         return self
-    
+
     def __exit__(self, exc_type, exc_value, traceback):
         self.terminate()
 
@@ -29,17 +30,17 @@ class Noise:
 
     def __enter__(self):
         return self
-    
+
     def __exit__(self, exc_type, exc_value, traceback):
         self.stream.stop_stream()
         self.stream.close()
 
     @staticmethod
-    def load_wav(wav, blockSize=1024):
-        data = wav.readframes(blockSize)
+    def load_wav(wav, chunkSize=1024):
+        data = wav.readframes(chunkSize)
         while data:
             yield data
-            data = wav.readframes(blockSize)
+            data = wav.readframes(chunkSize)
 
     def play(self, loop=True):
         for chunk in (cycle(self.loaded) if loop else self.loaded):
@@ -56,18 +57,28 @@ class Noise:
 
 class Playlist:
     def __init__(self, *sounds):
+        self.files = [*sounds]
         self.machine, self.sounds = Machine(), []
+        self.add(*self.files)
+
+    def scan(self, directory):
+        for f in scandir(directory):
+            if not f.is_dir()
+                self.files.append(f.path)
+
+    def add(self, *sounds):
         for sound in sounds:
             self.sounds.append(Noise(self.machine, sound))
 
     def start(self):
         with self.machine as machine:
-            self.sounds[0].play(loop=False)
+            try:
+                self.sounds[0].play(loop=True)
+            except KeyboardInterrupt:
+                return
 
 
-f = 'audio_files/pinkNoise_01.wav'
-#with Machine() as machine:
-#    with Noise(machine, f) as noise:
-#        noise.play(loop=True)
-p = Playlist(f)
-p.start()
+if __name__ == '__main__':
+    f = 'audio_files/pinkNoise_01.wav'
+    p = Playlist(f)
+    p.start()

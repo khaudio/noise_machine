ab9e880da09f62f1d2df540b3ae067af0d96296d
Kyle Hughes <kylehughes1@gmail.com>
2018-09-04 19:41:52 -0500

    renamed main to pyplayer and added new main to inherit from pyplaer

--- scipy_spectral.py
+++ scipy_spectral.py
@@ -2,6 +2,10 @@
 
 import scipy.io.wavfile as swav
 import numpy as np
+import wave
+import thinkdsp
+import thinkplot
+import os
 
 """Spectral manipulation sandbox using scipy and numpy"""
 
@@ -11,5 +15,21 @@ def process(filepath):
     for chunk in f[1]:
         print(chunk)
 
+
+def analyze(filepath, room):
+    wav = thinkdsp.read_wave(filepath).make_spectrum()
+    room = thinkdsp.read_wave(room).make_spectrum()
+
+    wav.plot()
+    room.plot()
+    thinkplot.show()
+
+
 if __name__ == '__main__':
-    process('./Other/audio_files/pinkNoise_01.wav')
+    directory = './Other/audio_files/'
+    signal = os.path.join(directory, 'pinkNoise_01.wav')
+    room = os.path.join(directory, 'pinkNoise_01_smallRoom.wav')
+    try:
+        analyze(signal, room)
+    except KeyboardInterrupt:
+        pass
